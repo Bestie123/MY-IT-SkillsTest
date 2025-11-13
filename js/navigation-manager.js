a4afcd91a59697028cf45aa4203474dbb266dfa8
Mikhail <misha1997-09@mail.ru>
2025-11-13 23:02:50 +0300

    Update navigation-manager.js

--- js/navigation-manager.js
+++ js/navigation-manager.js
@@ -6,22 +6,25 @@ const navigation = {
 
     updateBreadcrumb() {
         const breadcrumb = document.getElementById('breadcrumb');
-        breadcrumb.innerHTML = '<div class="breadcrumb-item"><a href="#" onclick="navigation.resetView()">Все технологии</a></div>';
+        if (!breadcrumb) {
+            console.error('Breadcrumb element not found');
+            return;
+        }
+
+        breadcrumb.innerHTML = '<span class="breadcrumb-item" onclick="navigation.resetView()">Все технологии</span>';
         
         if (this.currentView.path.length > 0) {
             let currentPath = [];
             let currentNode = techData.categories;
             
             for (const index of this.currentView.path) {
-                if (currentNode[index]) {
+                if (currentNode && currentNode[index]) {
                     currentPath.push(index);
                     const pathCopy = [...currentPath];
                     breadcrumb.innerHTML += `
-                        <div class="breadcrumb-item">
-                            <a href="#" onclick="navigation.viewNode(${JSON.stringify(pathCopy)})">
-                                ${currentNode[index].name}
-                            </a>
-                        </div>
+                        <span class="breadcrumb-item" onclick="navigation.viewNode(${JSON.stringify(pathCopy)})">
+                            ${currentNode[index].name}
+                        </span>
                     `;
                     currentNode = currentNode[index].children || [];
                 }
@@ -32,12 +35,12 @@ const navigation = {
     resetView() {
         this.currentView = { type: 'all', path: [] };
         this.updateBreadcrumb();
-        uiManager.renderTree();
+        uiManager.renderStructure();
     },
 
     viewNode(path) {
         this.currentView = { type: 'node', path: path };
         this.updateBreadcrumb();
-        uiManager.renderTree();
+        uiManager.renderStructure();
     }
 };
